d5a4b6aa3bf2c4f33c1db0e92e75078f4775031d
Peng Fan <peng.fan@nxp.com>
2018-09-24 19:51:33 +0800

    arm: add missing imx8.h
    Add missing imx8.h
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit ae711d2ac4d32612d8733ccbf4d01af544a8b48c)

---
+++ xen/include/asm-arm/imx8.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * Peng Fan <peng.fan@nxp.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __ASM_ARM_IMX8_H__
+#define __ASM_ARM_IMX8_H__
+
+#define MAX_GPIO_CONTROLLER 10
+
+struct gpio_dom {
+    struct {
+        paddr_t base;
+        u32 bits;
+    } gpios[MAX_GPIO_CONTROLLER];
+};
+
+int domain_vgpio_init(struct domain *d, struct gpio_dom *gpio_dom);
+int domain_vgpio_free(struct domain *d);
+
+#endif
