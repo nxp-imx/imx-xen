b57d509c0398e33b60e0b670a5ba05662147b9d7
Peng Fan <peng.fan@nxp.com>
2018-09-26 13:31:23 +0800

    libxl: add vi2c device
    Add vi2c device
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- tools/libxl/libxl_create.c
+++ tools/libxl/libxl_create.c
@@ -1501,6 +1501,7 @@ const struct libxl_device_type *device_type_tbl[] = {
     &libxl__pcidev_devtype,
     &libxl__dtdev_devtype,
     &libxl__vdispl_devtype,
+    &libxl__vi2c_devtype,
     NULL
 };
 
